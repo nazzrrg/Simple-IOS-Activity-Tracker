236909677d88a4c378e95da13075452ef7aead4b
Егор Назаров <nazarov.egor2010@yandex.ru>
2021-03-31 00:14:31 +0300

    initial commit

---
+++ Activity Tracker/Activity Tracker/Activity_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_TrackerApp.swift
+//  Activity Tracker
+//
+//  Created by Егор Назаров on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Activity_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Activity Tracker/Activity Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Activity Tracker
+//
+//  Created by Егор Назаров on 30.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
